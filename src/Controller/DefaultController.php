6d084db16346ee4a67874ccddc986834ccd5bafe
Antoine <antoinemanier3@gmail.com>
2019-10-21 14:23:39 +0200

    1er template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController {
+class DefaultController extends AbstractController {
+
+    /**
+     * @Route("/", name="index")
+     */
+
     public function index() {
         /**
          * Réponse simple
          * use ... \Response;
          */
-        return new Response('Mon premier Symfony !');
+        // return new Response('<body> Mon premier Symfony !</body>');
+
+        /**
+         * Retourne un template
+         */
+
+        return $this->render('default/home.html.twig');
     }
 }
 
