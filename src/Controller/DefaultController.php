bede055e72c46661d7ee42786b9ab1a5110489b7
Antoine <antoinemanier3@gmail.com>
2019-10-21 15:22:15 +0200

    controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -25,6 +25,35 @@ class DefaultController extends AbstractController {
 
         return $this->render('default/home.html.twig');
     }
+
+    /**
+    * @Route("/page-test-{id}.{ext}", 
+    *   name="test", 
+    *   requirements={"id"="\d+"}, 
+    *   defaults={"id": 1, "ext": "png"})
+    */
+
+     // mettre : {} dans une route : paramètre de route (comme au dessus) Route = créer l'URL
+
+    public function test($id, $ext) {
+        /**
+        * Réponse simple
+        * use ... \Response;
+        */
+        // return new Response('<body> Ma deuxième page Symfony !</body>');
+
+        /**
+         * Reponse avec paramètre
+         */
+
+        //  return new Response("id=" .$id. " et " .$ext);
+
+        return $this->render("default/test.html.twig",
+            [
+                'id' => $id,
+                'ext' => $ext
+            ]);
+    }
 }
 
 ?>
